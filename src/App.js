ecb0a0de4d68cfe0c5630d1be45ef502d85d0200
Dayoung Park <qkrekdud26@gmail.com>
2022-12-22 02:16:34 +0900

    :sparkles:feat(db.json): make initial data in db.json
    db.json 에 todos의 초기값을 넣었습니다.
    json-server를 연결시켰습니다.
    
    by @dypark26

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios"; // axios import 합니다.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [todos, setTodos] = useState(null);
+
+  const fetchTodos = async () => {
+    const { data } = await axios.get("http://localhost:3001/todos");
+    setTodos(data);
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  console.log(todos);
+  return <div>App</div>;
+};
 
 export default App;
